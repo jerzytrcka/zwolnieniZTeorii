599b1b687d6c6c983e27074bd6bfeee863f73d9b
Jerzy Trcka <jerzy.trcka@gmail.com>
2022-01-27 23:01:13 +0100

    Added header but it doesnt work

---
+++ kasa-to-klasa/src/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.component {
+  render() {
+    return (
+      <head>
+        <h1>Kasa to klasa</h1>
+          <nav><ul>
+            <li>Test</li>
+          </ul></nav>
+      </head>
+    );
+  }
+}
---
+++ kasa-to-klasa/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './header.js';
+
+ReactDOM.render(<Header />, document.getElementById('root'));
