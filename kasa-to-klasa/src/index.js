562a3dac3dbf6def771387fa1dc20c3aa59cf1fd
Jerzy Trcka <jerzy.trcka@gmail.com>
2022-02-13 17:48:58 +0100

    Unreacted react

--- kasa-to-klasa/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Header from './header.js';
-
-ReactDOM.render(<Header />, document.getElementById('root'));
